9bc472dfcff7ed95bbfed0b5d965dd451e4b0a9c
dka-li <57945737+dka-li@users.noreply.github.com>
2021-11-22 17:20:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='aciClient',
-      version='1.3',
+      version='1.4',
       description='aci communication helper class',
       url='http://www.netcloud.ch',
       author='mze',
       author_email='nc_dev@netcloud.ch',
       license='MIT',
       packages=['aciClient'],
-      install_requires=['requests>=2.25.0 , <3', 'pyOpenSSL>=19.1.0, <20'],
+      install_requires=['requests>=2.26.0 , <3', 'pyOpenSSL>=21.0.0, <22'],
       long_description=long_description,
       long_description_content_type='text/markdown',
       python_requires=">=3.6",
