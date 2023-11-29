a41cd2d08f41e038396f4e92b6bdf49ffd647a3a
Charlie Kim <kimcharli@gmail.com>
2023-11-29 14:10:48 -0500

    Added socks5 proxy support

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='aciClient',
-      version='1.5',
+      version='1.6',
       description='aci communication helper class',
       url='http://www.netcloud.ch',
       author='mze',
       author_email='nc_dev@netcloud.ch',
       license='MIT',
       packages=['aciClient'],
-      install_requires=['requests>=2.26.0 , <3', 'pyOpenSSL>=23.0.0, <24'],
+      install_requires=['requests[socks]>=2.26.0 , <3', 'pyOpenSSL>=23.0.0, <24', 'PySocks>=1.7.1, <2'],
       long_description=long_description,
       long_description_content_type='text/markdown',
       python_requires=">=3.6",
